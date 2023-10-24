2de75f80de8d3f91655a6fbc8e40ce902a562fd5
Quintiliano <quintiliano1995@gmail.com>
2023-10-24 10:58:57 -0300

    Criação de teste

--- test/screens/forms.screen.js
+++ test/screens/forms.screen.js
@@ -1,12 +1,21 @@
-// class FormsScreen {
-//     get formsButton() {
-//         return $('~Forms');
-//     }
+class FormsScreen {
+    get textInputFieldForms (){
+        return $('android=new UiSelector().text("Input field:")')
+    }
     
-//     async goToForms() {
-//         this.formsButton.click();
-//     }
-// }
+    get textField() {
+        return $('~text-input')
+    }
+    
+    // --
+    async isVisibleField(){
+        this.textInputFieldForms.waitForExist({timeout: 2000})
+    }
 
+    async fillOutForm() {
+        this.textField.setValue('Teste')
+        expect (await this.textField).toBeDisplayed()
+    }
+}
 
-// export default new FormsScreen();
+module.exports = new FormsScreen();
--- test/specs/start.spec.js
+++ test/specs/start.spec.js
@@ -1,12 +1,14 @@
 const HomeScreen = require('../screens/home.screen');
-const formsScreen = require('../screens/forms.screen');
+// const formsScreen = require('../screens/forms.screen');
 
 describe('Primeiro teste', () => {
+    var texto = 'Primeiro teste'
     it('Acessar o menu forms', async() => {
         await HomeScreen.goToForms();
         await $('android=new UiSelector().text("Input field:")').waitForExist({timeout: 2000})
-        await $('~text-input').setValue('teste')
+        await $('~text-input').setValue(texto);
         expect (await $('~text-input')).toBeDisplayed()
+
     });
 });
 
