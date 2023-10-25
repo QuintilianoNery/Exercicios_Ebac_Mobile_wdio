d3fbf91f86b1cf22255795559c44b0608cf89c8a
Quintiliano <quintiliano1995@gmail.com>
2023-10-25 16:46:55 -0300

    test forms

--- test/screens/forms.screen.js
+++ test/screens/forms.screen.js
@@ -1,20 +1,60 @@
 class FormsScreen {
-    get textInputFieldForms (){
-        return $('~text-input')
-    }
-    
+
     get textField() {
         return $('~text-input')
     }
 
-    async isVisibleField(){
-        this.textInputFieldForms.waitForExist({timeout: 2000})
+    get inputTextresult() {
+        return $('~input-text-result')
     }
 
-    async fillOutForm() {
-        this.textField.setValue()
-        expect (await this.textField).toBeDisplayed()
+    get clickSwitch() {
+        return $('~switch')
     }
+
+    get clickDropdown() {
+        return $('id:android_touchable_wrapper')
+    }
+
+    get selectOption() {
+        return $('android=Appium is awesome')
+    }
+
+    get clickButton() {
+        return $('~button-Active')
+    }
+    async isVisibleField() {
+        this.textField.waitForExist({ timeout: 2000 })
+    }
+
+    async fillInField(texto) {
+        this.textField.setValue()
+        expect(await this.textField).toBeDisplayed()
+    }
+
+    async clickTheSwitch() {
+        this.clickSwitch.click()
+    }
+
+    async clickTheDropdown() {
+        this.clickDropdown.click()
+
+    }
+
+    async clickTheButton() {
+        this.clickButton.click()
+    }
+
+    async clickInOption() {
+        this.selectOption.click()
+        // const text = await selectOption.getText();
+        // expect(text).to.equal('Appium is awesome')
+    }
+
+
+    // async getInputText(){
+    //     return await this.inputTextresult.getText();
+    // }
 }
 
 module.exports = new FormsScreen();
--- test/specs/start.spec.js
+++ test/specs/start.spec.js
@@ -1,12 +1,28 @@
 const HomeScreen = require('../screens/home.screen');
 const FormsScreen = require('../screens/forms.screen');
-
+let texto = 'Primeiro teste'
 describe('Primeiro teste', () => {
-    let texto = 'Primeiro teste'
-    it('Acessar o menu forms', async() => {
+    it('Acessar o forms', async() => {
         await HomeScreen.goToForms();
         await FormsScreen.isVisibleField();
-        await FormsScreen.fillOutForm(texto);
+        await FormsScreen.fillInField(texto);
+        // expect (await FormsScreen.getInputText()).toEqual(texto);
+        await FormsScreen.clickTheSwitch();
+        await FormsScreen.clickTheDropdown();
+        await FormsScreen.clickInOption();
+        await FormsScreen.clickTheButton();
+
+    });
+    it('Acessar o Forms sem page Objects', async() => {
+        await $('~Forms').click()
+        const text_input = await $('//android.widget.EditText[@content-desc="text-input"]')
+        await text_input.waitForExist({ timeout: 2000 });
+        await text_input.setValue(texto);
+        await $('~switch').click();
+        await $('id:android_touchable_wrapper').click();
+        await $('android=Appium is awesome').click();
+        await $('~button-Active').click();
+        
     });
 });
 
